6478e046fa7ef9110b4d4c956b38c9aefd160e8c
Quad72 <quad.heo@snu.ac.kr>
2024-09-11 16:00:51 +0900

    스타일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,90 @@ import './App.css';
 
 import { useState } from 'react';
 
-import viteLogo from '/vite.svg';
+import gjprofile from './assets/profile.png';
+//import viteLogo from '/vite.svg';
 
-import reactLogo from './assets/react.svg';
+//import reactLogo from './assets/react.svg';
 
 function App() {
   //const [count, setCount] = useState(0);
 
   const [fontSize, setFontSize] = useState(2);
+  const [gap, setgap] = useState(0);
+  const [il, setil] = useState('1');
 
   // 버튼 클릭 시 호출되는 함수
   const increaseFontSize = () => {
     setFontSize((prevSize) => parseFloat((prevSize + 0.2).toFixed(1))); // 글자 크기를 0.2em씩 증가
   };
+  //일 벌리기 함수
+  const seperatingone = () => {
+    setil('11');
+    setgap((prevgap) => prevgap + 2);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1 style={{ fontSize: '12px' }} className="centered-h1">
+          클릭하여 깃허브로 이동 -&gt;
+        </h1>
+        <div>
+          <a
+            href="https://github.com/Quad72"
+            target="_blank"
+            rel="noreferrer"
+            className="centered-content"
+          >
+            <img src={gjprofile} className="profile" alt="profile_fig" />
+          </a>
+        </div>
+        <h1 style={{ fontSize: '12px' }} className="centered-h1">
+          &lt;-클릭하여 깃허브로 이동
+        </h1>
       </div>
       <h1
         style={{
-          fontSize: `${fontSize}em`,
+          fontSize: `2em`,
           transition: 'font-size 0.3s ease',
+          marginTop: '1em',
+          marginBottom: '1em',
         }}
       >
         허용재
       </h1>
-      <div className="card">
-        <button onClick={increaseFontSize}>size is {fontSize}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="centered-list">
-        <div className="card">
+      <div className="centered-div">
+        <div className="card" style={{ fontSize: '0.8em' }}>
           <h1>안녕하세요!</h1>
         </div>
-        <ul>
-          <li>조선해양공학과 20학번</li>
-          <li>text2</li>
-          <li>text2</li>
+        <ul className="centered-list">
+          <li style={{ fontSize: '16px' }}>조선해양공학과 20학번</li>
+          <li style={{ fontSize: '16px' }}>MBTI는 ENTP입니다</li>
+          <li style={{ fontSize: '16px' }}>말장난 개그를 좋아하는 편입니다</li>
+          <li>
+            <p
+              className="heading"
+              style={{
+                fontSize: `16px`,
+              }}
+            >
+              이런거 말이죠
+            </p>
+          </li>
+          <li>
+            <button onClick={increaseFontSize}>일 키우기</button>
+            <button onClick={seperatingone}>일 벌리기</button>
+            <p
+              className="heading"
+              style={{
+                fontSize: `${fontSize}em`,
+                transition: 'font-size 0.3s ease',
+                letterSpacing: `${gap}px`,
+              }}
+            >
+              {il}
+            </p>
+          </li>
         </ul>
       </div>
     </>
